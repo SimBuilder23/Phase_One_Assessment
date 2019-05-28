1d0c90d7fe2022e08fa6540dac29f0e5744a0d56
Kyle Rippere <kylerippere@gmail.com>
2019-05-28 12:39:14 -0400

    users updated to have admin user as a separate table

--- 2-schema-designer/schema_designer_users.py
+++ 2-schema-designer/schema_designer_users.py
@@ -3,6 +3,7 @@
 import sqlite3
 
 def create_database():
+    ### creates table of regular users
     connection = sqlite3.connect("users.db", check_same_thread=False)
     cursor = connection.cursor()
 
@@ -16,12 +17,21 @@ def create_database():
         );"""
     )
 
-    cursor.close()
-    connection.close()
+
+    ### creates table of ADMIN USER... to keep ADMIN USER's data separate from regular users data  (i.e. partition of data)
+
+    cursor.execute(
+        """CREATE TABLE if not exists ADMIN(
+            pk INTEGER PRIMARY KEY AUTOINCREMENT,
+            Username VARCHAR,
+            Name VARCHAR,
+            Password TEXT,
+            Email TEXT
+        );"""
+    )
 
 
-    connection = sqlite3.connect("users.db", check_same_thread=False)
-    cursor = connection.cursor()
+    ### creates table of phone numbers for both users and admin
 
     cursor.execute(
         """CREATE TABLE if not exists PHONES(
@@ -34,4 +44,14 @@ def create_database():
     cursor.close()
     connection.close()
 
-create_database()    
+
+create_database()
+
+
+### My Logic:
+### Keep separate user tables for regular users and the admin, but can still use a single table for phone numbers (of all users)
+### Sample Queries:
+### 1. Want phone numbers for particular user:
+###     a. SELECT Phone from PHONES where Username = {some_username};
+### 2. Want the user's name to go with the above?
+###     a. SELECT users.name, phones.phone FROM USERS INNER JOIN PHONES ON USERS.username = PHONES.username;
